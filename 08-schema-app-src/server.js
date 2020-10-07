d785b1234b328ac8cb7a05b89dc13c59ed721c14
peterhchen <peter.hp.chen@gmail.com>
2020-10-07 12:06:37 -0700

    Add files via upload

---
+++ 08-schema-app-src/server.js
@@ -0,0 +1,44 @@
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const port = process.env.PORT||9000
+const app = express()
+
+const fs = require('fs')
+const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
+const resolvers = require('./resolvers')
+
+const {makeExecutableSchema} = require('graphql-tools')
+const schema = makeExecutableSchema({typeDefs, resolvers})
+
+app.use(cors(), bodyParser.json());
+
+const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
+app.use('/graphql',graphqlExpress({schema}))
+app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
+
+app.listen(
+   port, () => console.info(
+      `Server started on port ${port}`
+   )
+);
+
+// register middleware
+//app.use(bodyParser.json() , cors())
+// const typeDefinition = `
+// type Query  {
+//    greeting: String
+// }`
+// const  resolverObject = {
+//    Query : {
+//       greeting: () => 'Hello GraphQL  From TutorialsPoint !!'
+//    }
+// }
+// const schema = makeExecutableSchema({typeDefs:typeDefinition, resolvers:resolverObject})
+// const {graphqlExpress,graphiqlExpress} = require('apollo-server-express')
+// app.use(cors(), bodyParser.json());
+
+// app.use('/graphql',graphqlExpress({schema}))
+// app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
+// app.listen(port, () =>  console.log(`server is up and running ${port}`))
+
