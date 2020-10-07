21d5ce7089e8880076fea4e277b0f55b7024444b
peterhchen <peter.hp.chen@gmail.com>
2020-10-06 20:03:52 -0700

    Delete 01_server.js

--- 05-example-src/01_server.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require('body-parser')
-   const cors = require('cors')
-   const express = require('express')
-   const port = process.env.PORT|| 9000
-   const app = express()
-   
-   //register middleware
-   app.use(bodyParser.json() , cors())
-   app.listen(port, () =>  console.log(`server is up and running at ${port}`)
